ef97400df888eaf62b7c0d1a7bee79f59f0e5e76
chingwl <chingwl@126.com>
2018-11-27 14:49:59 +0800

    改进代码缩进

--- spring_helloworld/src/main/Java/soundsystem/CDPlayer.java
+++ spring_helloworld/src/main/Java/soundsystem/CDPlayer.java
@@ -2,17 +2,17 @@ package soundsystem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component  // 组件
 public class CDPlayer implements MediaPlayer {
-  private CompactDisc cd;
+	private CompactDisc cd;
 
-  @Autowired        // 自动装配
-  public CDPlayer(CompactDisc cd) {
-    this.cd = cd;
-  }
+	@Autowired  // 自动装配
+	public CDPlayer(CompactDisc cd) {
+		this.cd = cd;
+	}
 
-  public void play() {
-    cd.play();
-  }
+	public void play() {
+		cd.play();
+	}
 
 }
