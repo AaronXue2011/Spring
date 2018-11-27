d664d96a97f25ca960f13b2f01c8b48dfe3a0f84
chingwl <chingwl@126.com>
2018-11-27 14:49:37 +0800

    在Java中进行显式配置Spring

--- spring_helloworld/src/main/Java/soundsystem/CDPlayerConfig.java
+++ spring_helloworld/src/main/Java/soundsystem/CDPlayerConfig.java
@@ -4,5 +4,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan      // 该注解能够在spring中启用组件扫描，默认扫描与该配置类相同的包
-public class CDPlayerConfig { 
+public class CDPlayerConfig {
+	// 在Java中进行显式配置Spring
 }
